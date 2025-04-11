0bfab0b6d4df1aaa2e058d6a7bef2868c48e6a8b
athulya002 <athulyasuresh7276@gmail.com>
2025-04-11 17:30:36 +0530

    Updated register.php added input box for gender.

--- public/pages/register.php
+++ public/pages/register.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,7 +7,6 @@
     <link rel="stylesheet" type="text/css" href="../assets/commonstyles.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
-
 <body>
     <div class="wrapper">
         <form action="../../api/signup.php" method="post">
@@ -37,6 +35,39 @@
                 <input type="date" name="age" placeholder="Date of Birth" required>
             </div>
 
+            <div class="input-box" style="position: relative; width: 100%; margin: 15px 0;">
+                <select name="gender" required style="
+                    width: 100%;
+                    height: 50px;
+                    background: rgba(255, 255, 255, 0.2);
+                    border: none;
+                    border-radius: 12px;
+                    font-size: 16px;
+                    color: white;
+                    padding: 15px 14px;
+                    outline: none;
+                    appearance: none;
+                    -webkit-appearance: none;
+                    -moz-appearance: none;
+                ">
+                    <option value="" disabled selected>Select Gender</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">Other</option>
+                </select>
+                <i class='bx bx-male-female' style="
+                    position: absolute;
+                    right: 15px;
+                    top: 50%;
+                    transform: translateY(-50%);
+                    font-size: 22px;
+                    color: rgba(255, 255, 255, 0.7);
+                    pointer-events: none;
+                "></i>
+            </div>
+
+
+
             <button type="submit" class="btn">Sign Up</button>
 
             <div class="register-link">
@@ -45,5 +76,4 @@
         </form>
     </div>
 </body>
-
 </html>
